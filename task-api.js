41882dd1dc801ff3d1dff95547ec095e2e1cc663
ishtiakanam <iafahim68@gmail.com>
2021-09-02 13:48:49 +0600

    first commit

---
+++ task-api.js
@@ -0,0 +1,55 @@
+const loadSingleUser= () => {
+    fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(data => displaySingleUser(data.results[0]))
+}
+loadSingleUser()
+
+const displaySingleUser = user => {
+    // console.log(user)
+};
+
+//meal db
+const toggleSpinner = displayStyle =>{
+    document.getElementById('spinner').style.display = displayStyle;
+}
+const toggleSpinnerResult = displayStyle =>{
+    document.getElementById('meals').style.display = displayStyle;
+}
+const searchMeal = () =>{
+    const searchText = document.getElementById("search-field").value;
+    //display spinner
+    toggleSpinner("block");
+    toggleSpinnerResult("none")
+    loadMeals(searchText);
+    document.getElementById("search-field").value = "";
+}
+
+const loadMeals = searchText => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s= ${searchText}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMeals(data.meals))
+}
+
+
+
+const displayMeals = meals => {
+    const container = document.getElementById('meals')
+    container.textContent = "";
+    meals?.forEach(meal => {
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <h1>${meal.strMeal}</h1>
+        <p>${meal.strIngredient8 ? meal.strIngredient8:'Not available'}</p>
+        <button onClick ="loadMealDetail('${meal.strMeal}')">click me</button>
+        `
+        container.appendChild(div)
+    });
+    toggleSpinner('none');
+    toggleSpinnerResult('block')
+}
+loadMeals('fish');
+const loadMealDetail = mealName => {
+    console.log(mealName)
+}
